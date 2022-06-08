cd74017c8176fee05dc5ef73b189d89b9faa7539
vRuShIl13 <patelvk1@myumanitoba.ca>
2022-06-08 11:28:14 -0500

    added the properties of the node class and started creating the insert method in the redblacktree class

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,107 @@
-public class Node {
+/*
+Red Black Trees are trees that contain nodes.
+the nodes will store a number of information
+The nodes will  either be black or red in color.
+The node contains a key,left and right and a pointer to the parent
 
+NOTE: The node can only be red or black</>
+ */
+
+
+import java.util.Objects;
+
+public class Node {
+    private String value;
+    private int key;
+    private String color;
+    private Node right;
+    private Node left;
+    private Node pParent;
+
+
+    //skeleton constructor
+    public Node() {
+        value = "";
+        key= -1;
+        color = "";
+        right = null;
+        left = null;
+        pParent= null;
+
+    }
+
+    //second constructor
+    public Node(int key,String value ) {
+        this.value = value;
+        this.key = key;
+    }
+
+    //=========================================================================================================
+
+
+    //=========================================================================================================
+    //TOSTRING method that provides the details about the node.
+    public String toString() {
+        return "Node{" +
+                "value='" + value + '\'' +
+                ", key=" + key +
+                ", color='" + color + '\'' +
+                ", right=" + right +
+                ", left=" + left +
+                ", pParent=" + pParent +
+                '}';
+    }
+
+    //=========================================================================================================
+    //GETTERS AND SETTERS
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getpParent() {
+        return pParent;
+    }
+
+    public void setpParent(Node pParent) {
+        this.pParent = pParent;
+    }
+
+    //=========================================================================================================
 }
--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -1,2 +1,78 @@
+/*
+The is the main tree, it starts with 0 nodes ,and it will have the following functions:
+ 1. Insert
+ 2. Delete
+ 3. Rank
+ 4. Minimum
+ 5. Maximum
+ 6. Search
+ 7. Select
+ */
+
 public class RedBlackTree {
+    private Node root;
+    private Node nil;
+
+
+    //constructor
+    public RedBlackTree() {
+        root = null;
+        nil = null;
+    }
+
+
+
+    //normal insert in a bst
+    public void insert(int i, String v){
+        Node y = null;
+        Node x = root;
+
+        Node newNodeZ = new Node(i,v);
+
+        //finding the correct position where the newNode should be
+        //looping down the tree
+        while (x != null){
+            y=x;
+            if(i < x.getKey()){
+                x = x.getLeft();
+            }else{
+                x = x.getRight();
+            }
+        }
+        //once a dead end is found, the newNode is linked to the last node visited.
+        newNodeZ.setpParent(y);
+
+        //if y is null that means the tree was empty and this is the first insertion
+        if(y == null){
+            root = newNodeZ;
+        }else if (newNodeZ.getKey() < y.getKey()){
+            y.setLeft(newNodeZ);
+        }else{
+            y.setRight(newNodeZ);
+        }
+
+        //the node is just created, so it doesnt have any left or right children
+        //according to the laws of red black trees, a new node must be coloured red.
+        newNodeZ.setLeft(null);
+        newNodeZ.setRight(null);
+        newNodeZ.setColor("RED");
+
+        rbInsertFixup(newNodeZ);
+    }
+
+
+    private void rbInsertFixup(Node z){
+        Node y;
+        while(z.getpParent().getColor().equals("RED")){
+            if(z.getpParent() == z.getpParent().getpParent().getLeft()){
+               y = z.getpParent().getpParent().getRight();
+            }
+
+        }
+
+    }
+
+
+
+
 }
